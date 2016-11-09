ece772b516c0fb702864e45064e51733d2825366
Ben Larkins <ben.larkins.3@gmail.com>
2016-11-09 17:34:41 -0500

    Update index.js to export on one line

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export { addAlert, addTimedAlert, removeAlert } from './src/components/alert/redux';
-export Alert from './src/components/alert/Alert';
-export AlertContainer from './src/components/alert';
+import { addAlert, addTimedAlert, removeAlert } from './src/components/alert/redux';
+import Alert from './src/components/alert/Alert';
+import AlertContainer from './src/components/alert';
+
+export { addAlert, addTimedAlert, removeAlert, Alert, AlertContainer };
