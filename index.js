3c567f285728f5b043c22c10043b6583575307d5
John Bowen <john@roundsphere.com>
2016-11-21 17:20:09 -0500

    Add to exports.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { addAlert, addTimedAlert, removeAlert } from './src/components/alert/redux';
 import Alert from './src/components/alert/Alert';
 import AlertContainer from './src/components/alert';
+import HomePage from './src/components/HomePage';
 
-export { addAlert, addTimedAlert, removeAlert, Alert, AlertContainer };
+export {
+    addAlert,
+    addTimedAlert,
+    removeAlert,
+    Alert,
+    AlertContainer,
+    HomePage,
+};
