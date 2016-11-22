819a413de8729c81656de530d3ab2839da139114
John Bowen <john@roundsphere.com>
2016-11-21 20:57:33 -0500

    Do not ignore src/index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { addAlert, addTimedAlert, removeAlert } from 'components/alert/redux';
+import Alert from 'components/alert/Alert';
+import AlertContainer from 'components/alert';
+import HomePage from 'components/HomePage';
+
+export {
+    addAlert,
+    addTimedAlert,
+    removeAlert,
+    Alert,
+    AlertContainer,
+    HomePage,
+};
